faa9716be16346610cdcd707df5b5156a56728fb
chintald <67239488+chintald@users.noreply.github.com>
2023-02-12 18:59:38 -0500

    angle manipulation completed (#6)

--- strummate.cpp
+++ strummate.cpp
@@ -2,7 +2,9 @@
 Servo servo;
 
 int potPin = 0;             // analog pin that will be used to read the value of the potentiometer
+int speedPotPin = 1;        // analog pin that will be used to read the speed of the servo
 double potVal = 0;          // variable to store the value of the potentiometer which will tell which strings to strike
+double speedPotVal = 0;     // variable to store the speed of striking
 
 int pulseWidth, delayTime;  // Initialise variables to store the pulse width and delay time of the servo
 
@@ -14,6 +16,7 @@ void setup()
 {
     servo.attach(9);        // attach the servo to pin 9
     pinMode(A0, INPUT);     // set the first analog pin as input
+    pinMode(A1, INPUT);     // set the second analog pin as input
     servo.write(20);        // set the initial position of the servo to 20 degrees
 }
 
@@ -23,6 +26,7 @@ void setup()
 void loop()
 {
     potVal = analogRead(potPin);    // read the value of the potentiometer
+    speedPotVal = analogRead(speedPotPin);  // read the speed of the servo
 
     // a loop to strike first two strings
     while (potVal > 0 && potVal <= 340)
@@ -33,6 +37,8 @@ void loop()
             pulseWidth = map(i, 0, 180, 500, 2500);       // map the value of i to a pulse width for the servo
             servo.writeMicroseconds(pulseWidth);          // write the pulse width to the servo
 
+            speedPotVal = analogRead(speedPotPin);         // read the speed of the servo
+            delayTime = map(speedPotVal, 0, 1023, 100, 0); // map the speed of the servo to a delay time
             delay(delayTime);                  // delay the loop by the delay time
             potVal = analogRead(potPin);       // read the value of the potentiometer
         }
@@ -43,6 +49,8 @@ void loop()
             pulseWidth = map(i, 0, 180, 500, 2500);     // map the value of i to a pulse width for the servo
             servo.writeMicroseconds(pulseWidth);        // write the pulse width to the servo
 
+            speedPotVal = analogRead(speedPotPin);      // read the speed of the servo
+            delayTime = map(speedPotVal, 0, 1023, 100, 0);      // map the speed of the servo to a delay time
             delay(delayTime);                           // delay the loop by the delay time
             potVal = analogRead(potPin);                // read the value of the potentiometer
         }
@@ -56,6 +64,8 @@ void loop()
             pulseWidth = map(i, 0, 180, 500, 2500);     // map the value of i to a pulse width for the servo
             servo.writeMicroseconds(pulseWidth);        // write the pulse width to the servo
 
+            speedPotVal = analogRead(speedPotPin);      // read the speed of the servo
+            delayTime = map(speedPotVal, 0, 1023, 100, 0);      // map the speed of the servo to a delay time
             delay(delayTime);                           // delay the loop by the delay time
             potVal = analogRead(potPin);                // read the value of the potentiometer
         }
@@ -65,6 +75,8 @@ void loop()
             pulseWidth = map(i, 0, 180, 500, 2500);     // map the value of i to a pulse width for the servo
             servo.writeMicroseconds(pulseWidth);        // write the pulse width to the servo
 
+            speedPotVal = analogRead(speedPotPin);      // read the speed of the servo
+            delayTime = map(speedPotVal, 0, 1023, 100, 0);  // map the speed of the servo to a delay time
             delay(delayTime);                           // delay the loop by the delay time
             potVal = analogRead(potPin);                // read the value of the potentiometer
         }
@@ -79,6 +91,8 @@ void loop()
             pulseWidth = map(i, 0, 180, 500, 2500);     // map the value of i to a pulse width for the servo
             servo.writeMicroseconds(pulseWidth);        // write the pulse width to the servo
 
+            speedPotVal = analogRead(speedPotPin);      // read the speed of the servo
+            delayTime = map(speedPotVal, 0, 1023, 100, 0);  // map the speed of the servo to a delay time
             delay(delayTime);                           // delay the loop by the delay time
             potVal = analogRead(potPin);                // read the value of the potentiometer
         }
@@ -89,6 +103,8 @@ void loop()
             pulseWidth = map(i, 0, 180, 500, 2500);     // map the value of i to a pulse width for the servo
             servo.writeMicroseconds(pulseWidth);        // write the pulse width to the servo
 
+            speedPotVal = analogRead(speedPotPin);      // read the speed of the servo
+            delayTime = map(speedPotVal, 0, 1023, 100, 0);  // map the speed of the servo to a delay time
             delay(delayTime);                              // delay the loop by the delay time
             potVal = analogRead(potPin);                // read the value of the potentiometer
         }
